62d65c343e690130ed96a417ccda985007007727
Zero <liambocakra@gmail.com>
2019-07-08 22:42:09 +0800

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,9 +1,9 @@
 function playRandomSong() {
   var songs = [
-    "../music/ACHanime_Rachel_(CV._Haruka_Chisuga)_-_Pray.mp3",
-    "../music/[ACHanime]_Asaka_-_Marine_SNOW.mp3",
-    "../music/[ACHanime]_MYTH___ROID_-_VORACITY.mp3",
-    "../music/[ACHanime]_Sangatsu_no_Phantasia_-_Rubicon.mp3"
+    "https://anoaghost.github.io/assets/music/ACHanime_Rachel_(CV._Haruka_Chisuga)_-_Pray.mp3",
+    "https://anoaghost.github.io/assets/music/[ACHanime]_Asaka_-_Marine_SNOW.mp3",
+    "https://anoaghost.github.io/assets/music/[ACHanime]_MYTH___ROID_-_VORACITY.mp3",
+    "https://anoaghost.github.io/assets/music/[ACHanime]_Sangatsu_no_Phantasia_-_Rubicon.mp3"
   ];
   
   // Pick a random song.
