00d5feb0e2c6a1878faef356a62880b2d4a962dd
Zero <liambocakra@gmail.com>
2019-07-08 22:40:13 +0800

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,9 +1,9 @@
 function playRandomSong() {
   var songs = [
-    "file:///D:/MUSIK/yuiko%20-%20Petit%20Bonheur.mp3",
-    "file:///D:/MUSIK/Kamisama%20to%20Unmei%20Kakumei%20no%20Paradox~feat.%CE%BC's%20(Love%20Live!)/MP3/01.%20Kakumei%20desu%20ne,%20Kamisama!.mp3",
-    "file:///E:/Barusan/video/jp/[ACHanime]_Sangatsu_no_Phantasia_-_Rubicon.mp3",
-    "file:///E:/Barusan/video/M/MP3/[ACHanime]Sangatsu_no_Phantasia_-_Kaze_no_Koe_wo_Kiki_nagara.mp3"
+    "../music/ACHanime_Rachel_(CV._Haruka_Chisuga)_-_Pray.mp3",
+    "../[ACHanime]_Asaka_-_Marine_SNOW.mp3",
+    "../[ACHanime]_MYTH___ROID_-_VORACITY.mp3",
+    "../[ACHanime]_Sangatsu_no_Phantasia_-_Rubicon.mp3"
   ];
   
   // Pick a random song.
